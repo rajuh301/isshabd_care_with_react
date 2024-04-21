aee0e8576ad10f32be15b2aacc65971f29226a5d
Md. Nuruzzaman <rajuh301@gmail.com>
2024-04-21 20:19:52 +0600

    Fix client-side routing issue

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Output directory for the build
-    assetsDir: 'assets',  // Directory for static assets
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: './src/main.jsx'  // Entry point for your React app
+        main: './src/main.jsx'
       }
     }
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {}
   }
 });
