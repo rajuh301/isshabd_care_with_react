bf21094652d91a2029df66ad282d66435b92f603
Md. Nuruzzaman <rajuh301@gmail.com>
2024-04-21 20:06:21 +0600

    last update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './src/main.jsx'
-      }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {}
-  }
-});
+})
